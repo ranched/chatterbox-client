733b793ad6878b611ab0a35398c5192a10aa8a56
ranched <jasosmc@yahoo.com>
2017-12-01 14:44:13 -0600

    Changed config.exemple.js to config.js and added basic method to app.js.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,54 @@
 // YOUR CODE HERE:
+const app = {
+    init: function() {
+
+    } ,
+    fetch: function() {
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+        type: 'GET',
+        success: function (data) {
+            console.log('Messages received');
+            const $chats = $('#chats');
+            data.results.forEach((message) => {
+              let $message = $('<div class="chat"></div>');
+              $message.text(message.text);
+              $chats.append($message);
+            });
+        },
+        error: function (data) {
+            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+            console.error('chatterbox: Failed to send message', data);
+        }
+      });
+    },
+    send: function(messageObj) {
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+            console.log('chatterbox: Message sent');
+        },
+        error: function (data) {
+            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+            console.error('chatterbox: Failed to send message', data);
+        }
+      });
+    },
+    clearMessages: function() {
+
+    },
+    renderMessage: function(messageObj) {
+
+    },
+    renderRoom: function(roomTitle) {
+
+    },
+    handleUsernameClick: function() {
+
+    }
+}
