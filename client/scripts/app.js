02a32d48ee93064538e659ab08ae7b8ed7f0da90
ranched <jasosmc@yahoo.com>
2017-12-01 20:00:17 -0600

    Add clear method, start AJAX parameters

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,9 @@
 // YOUR CODE HERE:
 const app = {
+    selectedRoom: 'lobby',
     messageCount: 0,
+    lastestMessageDate: '',
+    server: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
     init: function() {
       $(document).ready(function() {
         app.fetch();
@@ -13,33 +16,49 @@ const app = {
       });
     } ,
     fetch: function(skip=0) {
-      $.ajax({
-        // This is the url you should use to communicate with the parse API server.
-        url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages?limit=200&skip=' + skip,
-        type: 'GET',
-        success: function (data) {
-            console.log('Messages received');
-            const $chats = $('#chats');
-            data.results.forEach((message) => {
-              app.messageCount++;
-              let $message = $('<div class="chat"></div>');
-              $message.text(message.text);
-              $chats.prepend($message);
-            });
-        },
-        error: function (data) {
-            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-            console.error('chatterbox: Failed to send message', data);
+        app.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
+        const first50 = [];
+        app.server += `&where={"roomname":{"$regex":"^${app.selectedRoom}"}}`;
+        
+        const ajaxRequestObject = {
+          // This is the url you should use to communicate with the parse API server.
+          url: app.server,
+          type: 'GET',
+          data: {
+            limit: 200,
+            order: "-createdAt",
+            skip: skip,
+            where: {
+              roomname: {
+                $regex: "^$" + app.selectedRoom 
+              }
+            }
+          },
+          success: function (data) {
+              app.lastestMessageDate = data.results[0].createdAt;
+              console.log('Messages received');
+              data.results.forEach((message) => {
+              app.renderMessage(message);
+              });
+              console.log(data);
+          },
+          error: function (data) {
+              // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+              console.error('chatterbox: Failed to send message', data);
+          }
         }
-      });
+        if(app.lastestMessageDate) {
+            app.server += `&where={"createdAt":{"$gte":${app.lastestMessageDate}}}`;
+        }
+      $.ajax(ajaxRequestObject);
     },
     send: function(messageObj) {       
       $.ajax({
         // This is the url you should use to communicate with the parse API server.
         url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
         type: 'POST',
-        data: JSON.stringify(messageObj),
-        contentType: 'application/json',
+        data: messageObj,
+        dataType: 'json',
         success: function (data) {
             console.log('chatterbox: Message sent');
         },
@@ -50,15 +69,23 @@ const app = {
       });
     },
     clearMessages: function() {
-
+        $('#chats').empty();
     },
     renderMessage: function(messageObj) {
-
+      app.messageCount++;
+      const $chats = $('#chats');
+      let $message = $('<div class="chat"></div>');
+      $message.text(messageObj.text);
+      $chats.append($message); 
     },
     renderRoom: function(roomTitle) {
 
     },
     handleUsernameClick: function() {
+
+    }, 
+    handleSubmit: function(event) {
+      event.preventDefault();
 
     }
 }
